4a02d9066e488c6db080c819514af06668dace9c
Malian <deronmalian@gmail.com>
2013-07-11 12:32:08 +0200

    Add NotFoundHttpException when anno doesn't exist

--- Controller/ComiteController.php
+++ Controller/ComiteController.php
@@ -13,8 +13,11 @@ namespace Asbo\WhosWhoBundle\Controller;
 
 use Asbo\WhosWhoBundle\Entity\Post;
 use Asbo\WhosWhoBundle\Util\AnnoManipulator;
+use Asbo\WhosWhoBundle\Validator\AnnoValidator;
+use Asbo\WhosWhoBundle\Validator\Constraints\Anno;
 use Asbo\ResourceBundle\Controller\ResourceController;
 use JMS\SecurityExtraBundle\Annotation\Secure;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Controller of comite page
@@ -44,8 +47,24 @@ class ComiteController extends ResourceController
 
     public function annoAction($anno)
     {
+        $error = $this->container->get('validator')->validateValue($anno, new Anno());
+
+        if (count($error) != 0) {
+            throw new NotFoundHttpException("Anno doens't exists");
+        }
+
         $fraHasPostManager = $this->container->get('asbo_whoswho.fra_has_post_manager');
-        $posts             = $fraHasPostManager->findByTypesAndYear(array(Post::TYPE_COMITE, Post::TYPE_CONSEIL), $anno);
+        $posts = $fraHasPostManager->findByTypesAndYear(array(Post::TYPE_COMITE, Post::TYPE_CONSEIL), $anno);
+
+        /**
+         * Lors de la phase de transition avec le synode il n'y a pas encore de comité
+         * La solution ici n'est pas correcte parce que l'url correspondra à l'année actuel
+         * alors qu'elle affichera l'année précédente. Il faut, je pense, faire une rediretion
+         * temporaire (307).
+         */
+        if (count($posts) == 0) {
+            return $this->annoAction(--$anno);
+        }
 
         return $this->renderResponse(
             'listByAnno.html',
