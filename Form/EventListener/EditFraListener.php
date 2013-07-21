355c0626c29495d79ccc54a5097be839f0200766
Malian <deronmalian@gmail.com>
2013-07-21 17:33:15 +0200

    Adding listener to FraType

---
+++ Form/EventListener/EditFraListener.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the ASBO package.
+ *
+ * (c) De Ron Malian <deronmalian@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Asbo\WhosWhoBundle\Form\EventListener;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Doctrine\ORM\EntityRepository;
+
+class EditFraListener implements EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::PRE_SET_DATA => 'preSetData',
+            FormEvents::POST_SET_DATA => 'postSetData',
+        );
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function preSetData(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (null === $data || null === $data->getId()) {
+            return;
+        }
+
+        $closure = function (EntityRepository $er) use ($data) {
+            return $er->createQueryBuilder('a')
+                ->where('a.fra = :fra')
+                ->setParameter('fra', $data);
+        };
+
+        $form->add('principalAddress', null, array('query_builder' => $closure));
+        $form->add('principalPhone', null, array('query_builder' => $closure));
+        $form->add('principalEmail', null, array('query_builder' => $closure));
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function postSetData(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (null === $data || null === $data->getId()) {
+            return;
+        }
+
+        if ($data->getAddresses()->isEmpty()) {
+            $form->remove('principalAddress');
+        }
+
+        if ($data->getPhones()->isEmpty()) {
+            $form->remove('principalPhone');
+        }
+
+        if ($data->getEmails()->isEmpty()) {
+            $form->remove('principalEmail');
+        }
+    }
+}
--- Form/Type/FraType.php
+++ Form/Type/FraType.php
@@ -17,6 +17,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
 use Asbo\WhosWhoBundle\Util\AnnoManipulator;
+use Asbo\WhosWhoBundle\Form\EventListener\EditFraListener;
 use Doctrine\ORM\EntityRepository;
 
 // @Todo: Changer ici par une interface parce que normalement on devrait pas devoir utiliser directement la classe
@@ -30,14 +31,23 @@ use Asbo\WhosWhoBundle\Entity\Fra;
  */
 class FraType extends AbstractType
 {
+    /**
+     * @var string $class
+     */
     private $class;
+
+    /**
+     * @var EditFraListener $editFraListener;
+     */
+    private $editFraListener;
 
     /**
      * @param string $class The Fra class name
      */
-    public function __construct($class)
+    public function __construct($class, EditFraListener $editFraListener)
     {
         $this->class = $class;
+        $this->editFraListener = $editFraListener;
     }
 
     /**
@@ -55,51 +65,13 @@ class FraType extends AbstractType
                 ->add('status', 'choice', array('choices' => Fra::getStatusList(), 'disabled' => true))
                 ->add('anno', 'choice', array('choices' => AnnoManipulator::getAnnos() ,'disabled' => true))
                 ->add('pontif', 'checkbox', array('required' => false, 'disabled' => true))
-
-                ->addEventListener(
-                    FormEvents::PRE_SET_DATA,
-                    function (FormEvent $event) {
-                        $data = $event->getData();
-                        $form = $event->getForm();
-
-                        // Lorsque le formulaire est créé, l'entité passée est vide
-                        if (null === $data || null === $data->getId()) {
-                            return;
-                        }
-
-                        $closure = function (EntityRepository $er) use ($data) {
-                            return $er->createQueryBuilder('a')
-                                      ->where('a.fra = :fra')
-                                      ->setParameter('fra', $data);
-                        };
-
-                        $form->add('principalAddress', null, array('query_builder' => $closure));
-                        $form->add('principalPhone', null, array('query_builder' => $closure));
-                        $form->add('principalEmail', null, array('query_builder' => $closure));
-                    }
-                )
-                ->addEventListener(
-                    FormEvents::POST_SET_DATA,
-                    function (FormEvent $event) {
-                        $data = $event->getData();
-                        $form = $event->getForm();
-
-                        // Lorsque le formulaire est créé, l'entité passée est vide
-                        if (null === $data || null === $data->getId()) {
-                            return;
-                        }
-
-                        if (0 === count($data->getAddresses())) {
-                            $form->remove('principalAddress');
-                        }
-                    }
-                )
-                ->add('fraHasPosts', 'collection', array('by_reference' => false, 'type' => 'asbo_type_fraHasPost',
-                    'allow_add' => true, 'allow_delete' => true, 'prototype' => true))
+                ->add('fraHasPosts', 'collection', array('by_reference' => false, 'type' => 'asbo_type_fraHasPost', 'allow_add' => true, 'allow_delete' => true, 'prototype' => true))
                 ->add('addresses', 'collection', array('by_reference' => false, 'type' => 'asbo_type_address', 'allow_add' => true, 'allow_delete' => true, 'prototype' => true))
                 ->add('emails', 'collection', array('by_reference' => false, 'type' => 'asbo_type_email', 'allow_add' => true, 'allow_delete' => true, 'prototype' => true))
                 ->add('phones', 'collection', array('by_reference' => false, 'type' => 'asbo_type_phone', 'allow_add' => true, 'allow_delete' => true, 'prototype' => true))
                 ->add('diplomas', 'collection', array('by_reference' => false, 'type' => 'asbo_type_diploma', 'allow_add' => true, 'allow_delete' => true, 'prototype' => true));
+
+        $builder->addEventSubscriber($this->editFraListener);
     }
 
     /**
