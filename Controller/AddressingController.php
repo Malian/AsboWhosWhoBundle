0a19c21d82951a655e3d8a5d03fd1043f67cb66f
Malian <deronmalian@gmail.com>
2013-07-23 19:54:19 +0200

    Moving up a function

--- Controller/AddressingController.php
+++ Controller/AddressingController.php
@@ -20,58 +20,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
 
 /**
- * Controller of comite page
+ * Controller of Addressing
  *
  * @author De Ron Malian <deronmalian@gmail.com>
- * @todo Trouver un moyen de gérer les droits et la correspondance des entités sans répétition
  */
 class AddressingController extends ResourceController
 {
-
-    /**
-     * Update an address.
-     *
-     * @ParamConverter("address", class="Asbo\WhosWhoBundle\Entity\Address", options={"id" = "address_id"})
-     * @PreAuthorize("#fra.getId() == #address.getFra().getId()")
-     */
-    public function updateAction(Fra $fra, Address $address, Request $request)
-    {
-        if ($address->getFra()->getId() !== $fra->getId()) {
-            throw $this->createNotFoundException(
-                'Link between Fra and Address not found !'
-            );
-        }
-
-        if (!$this->isGranted('ROLE_WHOSWHO_FRA_EDIT', $address->getFra())) {
-            throw new AccessDeniedException(
-                'You are not allowed to edit this fra !'
-            );
-        }
-
-        $form = $this->getAddressForm()->setData($address)->handleRequest($request);
-
-        if ($form->isValid()) {
-
-            $this->getAddressManager()->update($address);
-
-            $this->setFlash(
-                'success',
-                'Votre adresse a bien été modifiée !'
-            );
-
-            return $this->redirect($this->getFraController()->getFraEditUrl($fra));
-        }
-
-        return $this->renderResponse('update.html',
-            [
-                'fra'  => $fra,
-                'address' => $address,
-                'form' => $form->createView()
-            ]
-        );
-
-    }
-
     /**
      * Create an address
      */
@@ -106,18 +60,51 @@ class AddressingController extends ResourceController
     }
 
     /**
-     * Delete an address.
+     * Update an address.
      *
      * @ParamConverter("address", class="Asbo\WhosWhoBundle\Entity\Address", options={"id" = "address_id"})
+     * @PreAuthorize("#fra.getId() == #address.getFra().getId()")
      */
-    public function deleteAction(Fra $fra, Address $address, Request $request)
+    public function updateAction(Fra $fra, Address $address, Request $request)
     {
-        if ($address->getFra()->getId() !== $fra->getId()) {
-            throw $this->createNotFoundException(
-                'Link between Fra and Address not found !'
+        if (!$this->isGranted('ROLE_WHOSWHO_FRA_EDIT', $address->getFra())) {
+            throw new AccessDeniedException(
+                'You are not allowed to edit this fra !'
             );
         }
 
+        $form = $this->getAddressForm()->setData($address)->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $this->getAddressManager()->update($address);
+
+            $this->setFlash(
+                'success',
+                'Votre adresse a bien été modifiée !'
+            );
+
+            return $this->redirect($this->getFraController()->getFraEditUrl($fra));
+        }
+
+        return $this->renderResponse('update.html',
+            [
+                'fra'  => $fra,
+                'address' => $address,
+                'form' => $form->createView()
+            ]
+        );
+
+    }
+
+    /**
+     * Delete an address.
+     *
+     * @ParamConverter("address", class="Asbo\WhosWhoBundle\Entity\Address", options={"id" = "address_id"})
+     * @PreAuthorize("#fra.getId() == #address.getFra().getId()")
+     */
+    public function deleteAction(Fra $fra, Address $address, Request $request)
+    {
         if (!$this->isGranted('ROLE_WHOSWHO_FRA_EDIT', $fra)) {
             throw new AccessDeniedException(
                 'You are not allowed to edit this fra !'
